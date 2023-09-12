d720e2664f9b94c9c730cc7a7e7e38e46ba74876
veruyuschiyateist <lybomir.pavlick@gmail.com>
2023-09-12 10:37:58 +0300

    add onesignal

--- android/app/src/main/java/com/unicostudio/usa/war/amer/AviatorRedApp.kt
+++ android/app/src/main/java/com/unicostudio/usa/war/amer/AviatorRedApp.kt
@@ -1,6 +1,8 @@
 package com.unicostudio.usa.war.amer
 
 import android.app.Application
+import com.unicostudio.usa.war.amer.core.EngineInitializer
+import com.unicostudio.usa.war.amer.core.GameEngineInitializer
 
 
 class AviatorRedApp : Application() {
@@ -8,7 +10,7 @@ class AviatorRedApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        val initializer: EngineInitializer = GameEngineInitializer()
+        val initializer: EngineInitializer = GameEngineInitializer()
 //        initializer.initialize(this)
     }
 }
